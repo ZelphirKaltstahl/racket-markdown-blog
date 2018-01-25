fb3a5b1085afd50dfb0721f3c6fba5bcbbbbeae8
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2018-01-26 00:50:20 +0100

    contract added

--- blog/utils/string-operations.rkt
+++ blog/utils/string-operations.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(provide string-repeat)
+(provide (contract-out [string-repeat
+                        (-> natural? string? string?)]))
 
 (define (string-repeat n str)
   (apply string-append (make-list n str)))
